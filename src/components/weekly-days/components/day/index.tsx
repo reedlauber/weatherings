7b00d1d2780738f70e8e3b03d5831bc4e84adaf8
Reed Lauber <reedlauber@gmail.com>
2021-10-09 17:03:29 -0700

    Add temperate range swatches

--- src/components/weekly-days/components/day/index.tsx
+++ src/components/weekly-days/components/day/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import type { DayWeather } from 'types';
+
+import { toKelvin } from 'lib/temperature';
 
 import Temperature from 'components/temperature';
 
@@ -8,9 +10,30 @@ interface WeeklyDayProps {
   day: DayWeather;
 }
 
+
+const KELVIN_MIN = toKelvin(-5);
+const KELVIN_MAX = toKelvin(115);
+const KELVIN_SIZE = Math.abs(KELVIN_MAX - KELVIN_MIN);
+const HEIGHT = 16 * 5;
+
 const WeeklyDay = React.memo<WeeklyDayProps>(({ day }) => {
+  const styles = useMemo(() => {
+    const tempSize = day.high - day.low;
+    const relativeSize = tempSize / KELVIN_SIZE;
+    const bgSize = HEIGHT / relativeSize;
+
+    const maxDelta = KELVIN_MAX - day.high;
+    const offset = (bgSize / HEIGHT) * maxDelta * 2;
+
+    return {
+      '--bg-height': `${bgSize}px`,
+      '--pos-y': `-${offset}px`,
+    };
+  }, [day]);
+
   return (
-    <li key={day.name} className="weekly-day">
+    // @ts-ignore
+    <li key={day.name} className="weekly-day" style={styles}>
       <strong>{day.name}</strong>
 
       <div className="weekly-day-temperature">
